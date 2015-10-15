bc8c3fbde8e75f11faf8987af627ce5ea656f5ed
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-10-14 19:47:41 -0700

    Adds 'odd numbers' toy problem

---
+++ odd_numbers.js
@@ -0,0 +1,19 @@
+/*
+ODD NUMBERS
+CHALLENGE DESCRIPTION:
+
+
+Print the odd numbers from 1 to 99.
+
+INPUT SAMPLE:
+
+There is no input for this program.
+
+OUTPUT SAMPLE:
+
+Print the odd numbers from 1 to 99, one number per line. 
+*/
+
+for(var i = 1; i <= 99; i += 2){
+    console.log(i.toString() + "\n");
+}
