00c94ae95c0a79a17fd25e1887a17eb9ad6d75b4
Jack McDevitt <jackmcdevitt4@gmail.com>
2015-11-03 11:13:24 -0800

    Adds pangrams toy problem

---
+++ pangrams.js
@@ -0,0 +1,39 @@
+/*
+PANGRAMS
+CHALLENGE DESCRIPTION:
+
+
+The sentence 'A quick brown fox jumps over the lazy dog' contains every single letter in the alphabet. Such sentences are called pangrams. You are to write a program, which takes a sentence, and returns all the letters it is missing (which prevent it from being a pangram). You should ignore the case of the letters in sentence, and your return should be all lower case letters, in alphabetical order. You should also ignore all non US-ASCII characters.In case the input sentence is already a pangram, print out the string NULL
+
+INPUT SAMPLE:
+
+Your program should accept as its first argument a path to a filename. This file will contain several text strings, one per line. E.g.
+
+A quick brown fox jumps over the lazy dog
+A slow yellow fox crawls under the proactive dog
+OUTPUT SAMPLE:
+
+Print out all the letters each string is missing in lowercase, alphabetical order . E.g.
+
+NULL
+bjkmqz
+*/
+
+var checkP = function(str) {
+    var alph = "abcdefghijklmnopqrstuvwxyz", i = 0, curr;
+    alph = alph.split("");
+    str = str.reduce(function(x, y) {
+        curr = x.indexOf(y.toLowerCase());
+        if(curr !== -1) {
+            x.splice(curr, 1)
+        }
+        return x;
+    }, alph);
+    console.log(str.join("") || "NULL")
+}
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line != "") {
+            checkP(line.split(""));
+    }
+});
